8e007c2dee52b3afde4f7828ea5afea065b74c8b
Nael-Emin Ben Oun <benounnaelemin@gmail.com>
2026-03-12 17:00:14 +0100

    add password reset messages in German and English

---
+++ lang/de/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Dein Passwort wurde zurückgesetzt.',
+    'sent' => 'Wir haben dir einen Link zum Zurücksetzen per E-Mail gesendet.',
+    'throttled' => 'Bitte warte, bevor du es erneut versuchst.',
+    'token' => 'Dieser Link zum Zurücksetzen ist ungültig oder abgelaufen.',
+    'user' => 'Wir können keinen Benutzer mit dieser E-Mail-Adresse finden.',
+];
